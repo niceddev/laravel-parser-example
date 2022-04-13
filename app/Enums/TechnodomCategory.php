2f8f0ebca541c263a43393d88aae6221d555c536
sayrus383 <sayrus383@mail.ru>
2022-04-13 23:26:30 +0600

    Technodom parse

--- app/Enums/TechnodomCategory.php
+++ app/Enums/TechnodomCategory.php
@@ -4,7 +4,7 @@ namespace App\Enums;
 
 enum TechnodomCategory: string
 {
-    case LAPTOPS = 'https://api.technodom.kz/katalog/api/v1/products/category/noutbuki?city_id=5f5f1e3b4c8a49e692fefd70';
-    case COMPUTERS = 'https://api.technodom.kz/katalog/api/v1/products/category/stacionarnye-pk?city_id=5f5f1e3b4c8a49e692fefd70';
-    case LAPTOP_PC_ACCESSORIES = 'https://api.technodom.kz/katalog/api/v1/products/category/komp-juternye-aksessuary?city_id=5f5f1e3b4c8a49e692fefd70';
+    case LAPTOPS = 'https://api.technodom.kz/katalog/api/v1/products/category/noutbuki?city_id=5f5f1e3b4c8a49e692fefd70&limit=24';
+    case COMPUTERS = 'https://api.technodom.kz/katalog/api/v1/products/category/stacionarnye-pk?city_id=5f5f1e3b4c8a49e692fefd70&limit=24';
+    case LAPTOP_PC_ACCESSORIES = 'https://api.technodom.kz/katalog/api/v1/products/category/komp-juternye-aksessuary?city_id=5f5f1e3b4c8a49e692fefd70&limit=24';
 }
